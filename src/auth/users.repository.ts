8d46894c4f8878b6c005eb29940270452ad4813a
karlo <siladikarlo856@gmail.com>
2023-12-19 19:45:40 +0100

    feat: add sign up, validation and error handling

--- src/auth/users.repository.ts
+++ src/auth/users.repository.ts
@@ -1,10 +1,36 @@
-import { Injectable } from '@nestjs/common';
+import {
+  ConflictException,
+  Injectable,
+  InternalServerErrorException,
+} from '@nestjs/common';
 import { DataSource, Repository } from 'typeorm';
 import { User } from './user.entity';
+import { AuthCredentialsDto } from './dto/auth-credentials.dto';
 
 @Injectable()
 export class UsersRepository extends Repository<User> {
   constructor(private dataSource: DataSource) {
     super(User, dataSource.createEntityManager());
+  }
+
+  async createUser(authCredentialsDto: AuthCredentialsDto): Promise<void> {
+    const { username, password } = authCredentialsDto;
+
+    const user = this.create({
+      username,
+      password,
+    });
+
+    try {
+      await this.save(user);
+    } catch (error) {
+      console.log(error.code);
+      if (error.code === '23505') {
+        // duplicate username
+        throw new ConflictException('Username already exists');
+      } else {
+        throw new InternalServerErrorException();
+      }
+    }
   }
 }
