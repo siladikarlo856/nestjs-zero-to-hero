d347a9e42128a95c07ac1fefcec5ef5cd1438b35
karlo <siladikarlo856@gmail.com>
2023-12-21 19:10:03 +0100

    fix: fix getting all tasks search

--- src/tasks/task.repository.ts
+++ src/tasks/task.repository.ts
@@ -17,12 +17,13 @@ export class TaskRepository extends Repository<Task> {
 
     const query = this.createQueryBuilder('task');
     query.where({ user });
+
     if (status) {
       query.andWhere('task.status = :status', { status });
     }
     if (search) {
       query.andWhere(
-        'LOWER(task.title) LIKE LOWER(:search) OR LOWER(task.description) LIKE LOWER(:search)',
+        '(LOWER(task.title) LIKE LOWER(:search) OR LOWER(task.description) LIKE LOWER(:search))',
         { search: `%${search}%` },
       );
     }
