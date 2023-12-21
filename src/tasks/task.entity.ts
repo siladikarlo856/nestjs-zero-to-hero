e6fbd490bc380c77b087a7928856842a185fda46
karlo <siladikarlo856@gmail.com>
2023-12-21 19:02:07 +0100

    feat: serialize user data

--- src/tasks/task.entity.ts
+++ src/tasks/task.entity.ts
@@ -1,6 +1,7 @@
 import { Entity, PrimaryGeneratedColumn, Column, ManyToOne } from 'typeorm';
 import { TaskStatus } from './task-status.enum';
 import { User } from 'src/auth/user.entity';
+import { Exclude } from 'class-transformer';
 
 @Entity()
 export class Task {
@@ -17,5 +18,6 @@ export class Task {
   status: TaskStatus;
 
   @ManyToOne(() => User, (user) => user.tasks, { eager: false })
+  @Exclude({ toPlainOnly: true })
   user: User;
 }
